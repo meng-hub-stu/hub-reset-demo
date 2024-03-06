7f6428c93816f87aaa49f00fe3f54aa93d682e1a
meng-hub-stu <17611103114@163.com>
2024-03-06 22:16:02 +0800

    feat(hub-reset-spring): 学习spring
    学习demo

--- hub-reset-springboot/src/main/java/com/mdx/SpringBootApiApplication.java
+++ hub-reset-springboot/src/main/java/com/mdx/SpringBootApiApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2023/04/25
  */
 @SpringBootApplication
-public class ApiApplication {
+public class SpringBootApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(SpringBootApiApplication.class, args);
     }
 }
