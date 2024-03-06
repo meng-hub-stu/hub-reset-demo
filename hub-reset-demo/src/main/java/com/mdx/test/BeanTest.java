7867f39ce2f8ad1f98902ccab53605b4f19f6c9a
孟德磊 <mengdl@cheche365.com>
2024-03-06 17:54:38 +0800

    feat(demo): spring的bean的创建和周期

--- hub-reset-demo/src/main/java/com/mdx/test/BeanTest.java
+++ hub-reset-demo/src/main/java/com/mdx/test/BeanTest.java
@@ -1,6 +1,5 @@
 package com.mdx.test;
 
-import com.mdx.pojo.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,8 +10,8 @@ public class BeanTest {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("test.xml");
-        User user = applicationContext.getBean(User.class);
-        System.out.println(user.getName());
+        Emp emp = applicationContext.getBean(Emp.class);
+        System.out.println(emp.getName());
     }
 
 }
---
+++ hub-reset-demo/src/main/java/com/mdx/test/Emp.java
@@ -0,0 +1,24 @@
+package com.mdx.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * 测试bean生命周期
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Emp implements BeanFactoryAware, ApplicationContextAware {
+    private String name;
+
+    private BeanFactory beanFactory;
+
+    private ApplicationContext applicationContext;
+
+}
